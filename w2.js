d9f24fd3b89bf7fd594d6652b6fc35f7df6068ac
cyagbatolema <47867362+ocatarineta@users.noreply.github.com>
2019-05-08 16:08:07 +0300

    Add files via upload

---
+++ w2.js
@@ -0,0 +1,71 @@
+/*1*/
+console.log("greek // Γειά σου Κόσμε!");
+console.log("english // hello world!");
+
+/*2*/
+console.log('I'm awesome');
+console.log("I'm awesome"); /*solved, but undefined*/
+
+/*3*/
+var x;
+
+console.log("the value of my variable x will be: perrier");
+console.log(x);
+console.log("the value of my variable x will be: perrier");
+var x = "perrier";
+console.log(x);
+
+/*4*/
+var y = "Glitter";
+console.log(I think the value of my 'y' string is "Glitter");
+console.log("Glitter");
+console.log(y);
+var y = "mikro";
+console.log(I think the value of my 'y' string is "mikro");
+console.log("mikro");
+console.log(y);
+
+/*5*/
+noIdea
+
+/*6*/
+var array = ["Red", "Purple"];
+console.log(array);
+console.log(I think the value of my arrays are "Red" and "Purple");
+console.log("Red", "Purple");
+var  =["Elephant", "Alpaca"];
+console.log(favoriteAnimalsArray);
+console.log(favoriteAnimalsArray.add["baby pig"]);
+console.log(favoriteAnimalsArray);
+
+/*7*/
+let myString = "this is a test";
+console.log(myString);
+console.log(myString.length);
+
+/*8*/
+if (color1 == color2) {
+    console.log(print("Purple"));
+}
+else {
+    console.log(print("Red"));
+};
+
+let color1 = "Purple";
+let color2 = "Red";
+
+console.log("The value of my variable color is:" + "Purple");
+console.log("The value of my variable color is:" + "Red");
+
+/*9*/
+
+x = 7;
+x = x % 3;
+console.log(x);
+1
+
+
+/*10*/
+1
+var myArray = ["Animal" [5 , "100" ["Mammal"]]];
+2
